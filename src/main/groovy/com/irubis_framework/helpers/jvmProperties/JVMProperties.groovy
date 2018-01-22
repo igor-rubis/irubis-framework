1e100d0862c0a9c1232a892d1f5226dd6b5043a4
igor-rubis <rubis.igor@gmail.com>
2018-01-22 19:08:29 +0200

    refactoring

--- src/main/groovy/com/irubis_framework/helpers/jvmProperties/JVMProperties.groovy
+++ src/main/groovy/com/irubis_framework/helpers/jvmProperties/JVMProperties.groovy
@@ -4,7 +4,7 @@ class JVMProperties {
     static BROWSER = System.getProperty('browser', 'firefox')
     static API_PROXY = System.getProperty('apiProxy')
     static URL = System.getProperty('url')
-    static TESTS_MODE = System.getProperty('testsMode')
+    static TESTS_MODE = System.getProperty('testsMode', 'local')
     static UI_HUB_URL = System.getProperty('uiHubUrl')
     static MOBILE_HUB_URL = System.getProperty('mobileHubUrl')
 }
