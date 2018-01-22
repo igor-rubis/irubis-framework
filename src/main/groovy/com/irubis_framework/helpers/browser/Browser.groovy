2141181e42f90fc1d44ff12b5931479df84d00d0
igor-rubis <rubis.igor@gmail.com>
2018-01-22 18:48:42 +0200

    Removed usage of ThreadSafeObject.groovy

--- src/main/groovy/com/irubis_framework/helpers/browser/Browser.groovy
+++ src/main/groovy/com/irubis_framework/helpers/browser/Browser.groovy
@@ -5,50 +5,52 @@
 
 package com.irubis_framework.helpers.browser
 
-
-import com.irubis_framework.helpers.threadSafeObject.ThreadSafeObject
 import com.machinepublishers.jbrowserdriver.JBrowserDriver
 import com.machinepublishers.jbrowserdriver.Settings
 import com.machinepublishers.jbrowserdriver.UserAgent
 import io.github.bonigarcia.wdm.ChromeDriverManager
+import io.github.bonigarcia.wdm.FirefoxDriverManager
 import org.openqa.selenium.WebDriver
 
 /**
  * Created by Igor_Rubis. 8/3/16.
  */
 class Browser {
-    private static webDriver
+    private static FIREFOX = 'firefox'
+    private static CHROME = 'chrome'
+    private static J_BROWSER = 'jBrowser'
+    private static WEB_DRIVER
 
     private Browser() {}
 
     static WebDriver getInstance() {
-        if (!webDriver) {
+        if (!WEB_DRIVER) {
             def drvr = System.getProperty('browser')
+            def initializeWebDriver
 
             try {
-                def clazz
-                def coercionClass
                 switch (drvr) {
-                    case ['firefox', 'chrome'] : (clazz, coercionClass) = ["org.openqa.selenium.${drvr}.${drvr.capitalize()}Driver", "org.openqa.selenium.${drvr}.${drvr.capitalize()}Driver"]; break
-                    case 'jBrowser' : (clazz, coercionClass) = ["""${JBrowserDriver.canonicalName}(
+                    case [FIREFOX, CHROME] : initializeWebDriver = { Eval.me("return new org.openqa.selenium.${drvr}.${drvr.capitalize()}Driver()") }; break
+                    case J_BROWSER : initializeWebDriver = { Eval.me("""return new ${JBrowserDriver.canonicalName}(
                                                                     ${Settings.canonicalName}.builder()
                                                                         .hostnameVerification(false)
                                                                         .userAgent(${UserAgent.canonicalName}.CHROME)
-                                                                        .build())""", JBrowserDriver.canonicalName]; break
+                                                                        .build())""") }; break
                 }
-                webDriver = ThreadSafeObject.create(clazz, coercionClass)
+                WEB_DRIVER = initializeWebDriver()
             } catch (IllegalStateException ignored) {
                 switch (drvr) {
-                    case 'chrome': ChromeDriverManager.getInstance().setup(); break
+                    case CHROME: ChromeDriverManager.getInstance().setup(); break
+                    case FIREFOX: FirefoxDriverManager.getInstance().setup(); break
                 }
                 getInstance()
             }
         }
-        return webDriver
+        return WEB_DRIVER
     }
 
     static clear() {
-        !webDriver ?: webDriver.quit()
-        webDriver = null
+        !WEB_DRIVER ?: WEB_DRIVER.quit()
+        WEB_DRIVER = null
     }
 }
