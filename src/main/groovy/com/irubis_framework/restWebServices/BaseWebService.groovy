d85747a22bf42c66b1d8ae9db13a5dc8fc018786
igor-rubis <rubis.igor@gmail.com>
2018-02-16 11:16:53 +0200

    Extended BaseWebService class

--- src/main/groovy/com/irubis_framework/restWebServices/BaseWebService.groovy
+++ src/main/groovy/com/irubis_framework/restWebServices/BaseWebService.groovy
@@ -7,6 +7,7 @@ package com.irubis_framework.restWebServices
 
 import com.google.appengine.api.urlfetch.HTTPMethod
 import com.irubis_framework.helpers.jvmProperties.JVMProperties
+import groovy.json.JsonBuilder
 import org.apache.http.HttpHost
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
@@ -14,6 +15,7 @@ import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.impl.conn.DefaultProxyRoutePlanner
 import org.apache.http.util.EntityUtils
+import ru.yandex.qatools.allure.annotations.Attachment
 import wslite.json.JSONObject
 
 import java.util.logging.Logger
@@ -56,6 +58,12 @@ class BaseWebService {
         (request as HttpPost).setEntity(input)
     }
 
+    def setHeaders(headers) {
+        headers.each { key, value ->
+            request.setHeader(key, value)
+        }
+    }
+
     def analyzeResponseStatusCode(closure = null) {
         log.info("Request:  ${request}")
         response = client.execute(request)
@@ -64,5 +72,32 @@ class BaseWebService {
             responseJSON = new JSONObject(EntityUtils.toString(response.getEntity()))
             closure()
         }
+    }
+
+    @Attachment(value = 'Request and response info', type = 'application/json')
+    def dumpRequestResponseInfo() {
+        def info = [
+                request : [
+                        request_method : request.method,
+                        request_url    : request.URI.toString(),
+                        request_headers: request.headergroup.headers.collect { it.toString() },
+                        request_json   : requestJSON ?: 'No request json'
+                ],
+                response: [
+                        response_status_code: "${response.statusLine.statusCode} '${response.statusLine.reasonPhrase}'",
+                        response_headers    : response.original.headergroup.headers.collect {
+                            it.toString()
+                        },
+                        response_body       : responseJSON ?: responseBody
+                ]
+        ]
+
+        def stringifiedInfo
+        try {
+            stringifiedInfo = new JsonBuilder(info).toPrettyString()
+        } catch (OutOfMemoryError error) {
+            stringifiedInfo = "Could not dump request and response info due to error: ${error.message}"
+        }
+        return stringifiedInfo
     }
 }
