e1459214751e3baafbf9a057127834a2df4600d9
igor-rubis <rubis.igor@gmail.com>
2018-03-28 13:27:47 +0300

    added new page steps methods

--- src/main/groovy/com/irubis_framework/steps/webUiSteps/pageLevelSteps/PageSteps.groovy
+++ src/main/groovy/com/irubis_framework/steps/webUiSteps/pageLevelSteps/PageSteps.groovy
@@ -22,6 +22,10 @@ abstract class PageSteps extends WebUiSteps {
         return Browser.instance.findElement(by)
     }
 
+    protected evaluateJavascript(script, Object... args) {
+        ((JavascriptExecutor) Browser.getInstance()).executeScript(script, args)
+    }
+
     protected clickElement(By by) {
         eventually() {
             element(by).click()
@@ -36,12 +40,38 @@ abstract class PageSteps extends WebUiSteps {
         }
     }
 
+    protected jsClickElement(By by, interval = INTERVAL) {
+        eventually(interval) {
+            evaluateJavascript('arguments[0].click();', element(by))
+        }
+    }
+
+    protected String getElementAttribute(by, attr, interval = INTERVAL) {
+        eventually(interval) {
+            element(by).getAttribute(attr)
+        }
+    }
+
+    protected String getElementClassAttribute(by, interval = INTERVAL) {
+        getElementAttribute(by, 'class', interval)
+    }
+
+    protected String getElementSrcAttribute(by, interval = INTERVAL) {
+        getElementAttribute(by, 'src', interval)
+    }
+
     protected goToUrl(String url) {
         Browser.instance.navigate().to(url)
     }
 
     protected String getElementText(By by) {
         return ((JavascriptExecutor) Browser.instance).executeScript('return arguments[0].innerHTML', element(by)).toString().replaceAll('\n', '').replaceAll('\t', '').trim()
+    }
+
+    def getElementTextByJs(by, interval = INTERVAL) {
+        eventually(interval) {
+            ((JavascriptExecutor) Browser.getInstance()).executeScript('return arguments[0].value', element(by))
+        }
     }
 
     def typeInto(By by, String text) {
@@ -57,7 +87,24 @@ abstract class PageSteps extends WebUiSteps {
             Browser.instance.switchTo().frame(element(by))
         }
     }
+
     def getcurrentUrl() {
         Browser.instance.currentUrl
+    }
+
+    def isDisplayedElement(by) {
+        try {
+            element(by).isDisplayed()
+        } catch (org.openqa.selenium.NoSuchElementException ignored) {
+            return false
+        }
+    }
+
+    def waitForElementToAppear(by) {
+        eventually() {
+            if (!isDisplayedElement(by)) {
+                throw new AssertionError('Element did not appear on the page')
+            }
+        }
     }
 }
