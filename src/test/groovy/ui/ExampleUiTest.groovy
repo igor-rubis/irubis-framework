f9b8af72c67f76de17b62d2bf74dd87925db9cf8
igor-rubis <rubis.igor@gmail.com>
2018-04-06 19:21:03 +0300

    testing 'failOnBrowserConsoleError'

---
+++ src/test/groovy/ui/ExampleUiTest.groovy
@@ -0,0 +1,19 @@
+package ui
+
+import com.irubis_framework.helpers.browser.Browser
+import com.irubis_framework.helpers.testing.test.ui.BaseUiTest
+import org.junit.Test
+
+class ExampleUiTest extends BaseUiTest {
+    @Test
+    void failingTest() {
+        Browser.instance.get('https://www.google.com/')
+        assert (Browser.instance.getTitle() == '#######')
+    }
+
+    @Test
+    void passingTest() {
+        Browser.instance.get('https://www.google.com/')
+        assert (Browser.instance.getTitle() == 'Google')
+    }
+}
